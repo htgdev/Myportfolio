d60ee9617d442d1fde148a93b1905f936b1017c1
Mkharzom <htgdevstest@gmail.com>
2025-04-15 13:51:27 +0800

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-  base: '/Myportfolio/', // 👈 MUST match the repo name exactly
-  plugins: [vue()],
-})
